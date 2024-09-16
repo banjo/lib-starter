976596b3df5905f70293917c18a2036b1788abf3
Anton Ödman <anton.odman@gmail.com>
2024-09-16 20:51:53 +0200

    chore: eslint and typescript update

---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+import js from "@banjoanton/eslint-config/javascript";
+import ts from "@banjoanton/eslint-config/typescript";
+
+/** @type {import("eslint").Linter.Config} */
+export default [...js, ...ts];
